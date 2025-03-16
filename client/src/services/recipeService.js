0a562a3b8af9517630cb9eaa7562e6e1f0954603
Kristin <kristindyulgeryan@abv.bg>
2025-03-17 00:49:58 +0200

    Add abstract requester

--- client/src/services/recipeService.js
+++ client/src/services/recipeService.js
@@ -1,15 +1,25 @@
+import request from "../utils/request.js";
+
 const baseUrl = "http://localhost:3030/jsonstore/recipes";
 
 export default {
-  async create(recipeData) {
-    const response = await fetch(baseUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(recipeData),
-    });
-    const result = await response.jsone();
-    return result;
+  async getAll() {
+    const result = await request.get(baseUrl);
+    const recipes = Object.values(result);
+    return recipes;
+  },
+  async create(newRecipe) {
+    return request.post(baseUrl, newRecipe);
   },
 };
+
+// const response = await fetch(baseUrl, {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(newRecipe),
+// });
+
+// const result = await response.json();
+// console.log(result);
