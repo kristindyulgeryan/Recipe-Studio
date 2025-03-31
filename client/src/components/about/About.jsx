a9d6775e21a08c2a845e026cc325a5aebcf2f164
Kristin <kristindyulgeryan@abv.bg>
2025-03-31 17:15:42 +0300

    Add React image carousel to About section

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,34 +1,58 @@
+import { useState } from "react";
+import { images, descriptions } from "./aboutData.js";
+import {ArrowLeft, ArrowRight} from 'lucide-react';
+import style from './About.module.css'
+
+const getRandomNumber = ()=>Math.floor(Math.random() * 41) - 20;
+
+
 export default function About() {
+ const [index, setIndex]= useState(0)
+
+  
   return (
-    <section className="about-sec py-5">
+    <section className={`${style.aboutSec} py-5`}>
       <div className="container">
-        <div className="about row">
-          <div className="col-lg-6 col-md-12">
-            <div className="about-img">
-              <img src="imgs/family.png" alt="" />
+        <div className={`${style.about} row position-relative `}>
+          <div className="col-lg-6 col-md-12 ">
+            <div className={style.aboutImg}>
+              {images.map((image, i) => (
+                <img 
+                  key={i} 
+                  src={image} 
+                  alt={`About us ${i}`}
+                  className={`${style.galleryImage} ${
+                    i === index ? style.activeImg : style.inactiveImg
+                  }`}
+                  style={{
+                    transform: `rotate(${index === i ? 0 : getRandomNumber()}deg)`,
+                   
+                  }}
+                />
+              ))}
             </div>
           </div>
           <div className="col-lg-6 col-md-12">
-            <p className="title-p">About Us</p>
-            <h2 className="title-h">
+            <p className={style.titleP}>About Us</p>
+            <h2 className={style.titleH}>
               Nurturing Health Through Homemade Cooking
             </h2>
-            <p>
-              At Recipe Studio, we believe in the power of homemade food to
-              bring families and friends together while promoting health and
-              wellness. Our mission is to provide simple, nourishing recipes
-              that you can make at home, using fresh ingredients that are good
-              for your body and soul.
-            </p>
-            <p>
-              We are committed to creating a platform where everyone can find
-              easy-to-follow recipes, whether you're looking for a quick
-              weeknight dinner or a special meal to share with loved ones. Our
-              focus is on quality, authenticity, and supporting healthy
-              lifestyles—one homemade dish at a time!
-            </p>
+            <div className={style.descriptions}>
+              {descriptions.map((description, i) => (
+                <p key={i} className={`${i === index ? style.activeDesc : style.inactiveDesc}`}>{description}</p>
+              ))}
+            </div>
           </div>
+          <div className={style.arrowContainer}>
+          <button className={style.arrowButton} onClick={()=>setIndex((prev)=> prev === 0 ? images.length - 1 : prev-1)}>
+            <ArrowLeft size={18} />
+          </button>
+          <button className={style.arrowButton} onClick={()=>setIndex((prev)=> prev === images.length -1 ? 0 : prev + 1)}>
+            <ArrowRight size={18}/>
+          </button>
         </div>
+        </div>
+      
       </div>
     </section>
   );
